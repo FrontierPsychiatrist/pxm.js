13a4a4c09e1e4d4b46ca1fd068f78c3e8ef1dee3
Moritz Schulze <moritz.schulze@gmail.com>
2013-07-19 19:15:53 +0200

    Connection to database established. First method, /boards, implemented

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.database = {};
+config.database.url = '192.168.178.28';
+config.database.port = '3306';
+config.database.username = 'pxm';
+config.database.password = 'pxm';
+config.database.database = 'pxmboard';
+
+module.exports = config;
--- pxm.js
+++ pxm.js
@@ -1,11 +1,40 @@
+var config = require('./config');
 var express = require('express');
 var pxm = express();
 
+var mysql      = require('mysql2');
+var db = mysql.createConnection({ 
+  host: config.database.url,
+  port: config.database.port, 
+  user: config.database.username, 
+  password: config.database.password,
+  database: config.database.database
+});
+
+db.connect(function(err) {
+  if(err) {
+    console.log(err);
+    exit(1);
+  }
+});
+
 pxm.get('/', function(req, res) {
-  var body = 'PXM.JS';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+  res.send(pxm.routes);
+});
+
+pxm.get('/boards', function(req, res) {
+  var body = [];
+  db.query('SELECT * FROM pxm_board', function(err, rows, fields) {
+    for(var i = 0; i < rows.length; i++) {
+      body.push({
+        id: rows[i].b_id,
+        name: rows[i].b_name,
+        description: rows[i].b_description
+      });
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(body);
+  });
 });
 
 
