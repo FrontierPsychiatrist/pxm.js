41c4dbccb530e1c9ce3732c943362dc5c4cf5f37
Moritz Schulze <moritz.schulze@gmail.com>
2013-07-21 18:48:14 +0200

    Skeleton for posting a new thread implemented

--- pxm.js
+++ pxm.js
@@ -39,6 +39,14 @@ function defaultValue(value, _default) {
   return typeof value !== 'undefined' ? value : _default;
 }
 
+/**
+ * Standard return for a GET method. Either sends an object or an array (parameter useArray)
+ * @param err A JavaScript Error object that may have occured
+ * @param rows The rows to return in the response
+ * @param res The http response object
+ * @param useArray if true, all rows will be sent. Otherwise only the first row will be sent. Be careful, only
+ *  use useArray = true if you are sure there is only one row, otherwise nothing will be sent! 
+ **/
 function standardReturn(err, rows, res, useArray) {
   if(err) {
     res.send(500, err.message);
@@ -52,6 +60,31 @@ function standardReturn(err, rows, res, useArray) {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.send(body);
   }
+}
+
+/**
+ * Check if the given username and password are valid and then call either a success or error callback
+ * @param success A callback to be called when the login is possible with the given data. It will be called with the
+ *  selected userdata.
+ * @param error A callback to be called when the login was errornous.
+ **/
+function checkLogin(username, password, success, error) {
+  var md5 = crypto.createHash('md5');
+  md5.update(password);
+  var hashedPassword = md5.digest('hex');
+  db.execute('SELECT u_id, u_nickname FROM pxm_user WHERE u_nickname = ? AND u_password = ?',
+    [username, hashedPassword],
+    function(err, rows) {
+    if(err) {
+      throw err;
+    } else {
+      if(rows.length !== 1) {
+        error();
+      } else {
+        success(rows[0]);
+      }
+    }
+  });
 }
 
 pxm.get('/api/1', function(req, res) {
@@ -109,30 +142,100 @@ pxm.get('/api/1/message/:messageid', function(req, res, next) {
 });
 
 pxm.post('/api/1/login', function(req, res, next) {
-  var md5 = crypto.createHash('md5');
-  md5.update(req.body.password);
-  var hashedPassword = md5.digest('hex');
-  db.execute('SELECT u_id, u_nickname FROM pxm_user WHERE u_nickname = ? AND u_password = ?',
-    [req.body.username, hashedPassword],
-    function(err, rows) {
-      if(err) {
-        res.send(500);
-      } else {
-        if(rows.length !== 1) {
-          res.send(403);
-        } else {
-          req.session.authenticated = true;
-          req.session.userid = rows[0].u_id;
-          req.session.nickname = rows[0].u_nickname;
-          res.send(200);
-        }
-      }
-    });
+  var success = function(userdata) {
+    req.session.authenticated = true;
+    req.session.userid = userdata.u_id;
+    req.session.nickname = userdata.u_nickname;
+    res.send(200);
+  };
+  var error = function() {
+    res.send(401);
+  };
+  if(req.body.username && req.body.password) {
+    checkLogin(req.body.username, req.body.password, success, error);
+  } else {
+    res.send(400, 'Please provide the username and password parameter');
+  }
 });
 
 pxm.post('/api/1/logout', function(req, res, next) {
   req.session = null;
   res.send(200);
+});
+
+/**
+ * Create a thread
+ **/
+pxm.post('/api/1/board/:boardid/thread', function(req, res, next) {
+  var post = function() {
+    //TODO: required params checking
+    db.execute('INSERT INTO pxm_thread (t_boardid, t_active, t_lastmsgtstmp VALUES (?,?,?)',
+    [req.params.boardid, 1, new Date().getTime()],
+    function(err, result) {
+      if(err) {
+        res.send(500, err.message);
+      } else {
+        db.execute('INSERT INTO pxm_message ' +
+          '(m_threadid,m_parentid,m_userid,m_usernickname,m_usermail,m_userhighlight,m_subject,m_body,m_tstmp,m_ip,m_notification)',
+          'VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+          [result.insertId, req.body.parent_id, 'username?', 'usermail?', 'userhighlight?',
+            req.body.subject, req.body.body, new Date().getTime(), 'ip?', req.body.notification],
+          function(err, mResult) {
+            if(err) {
+              //TODO delete from pxm_thread
+              res.send(500);
+            } else {
+              db.execute('UPDATE pxm_board SET b_lastmsgtstmp = ? WHERE b_id = ?',
+                ['msgtstmp', req.params.boardid], function(err, response) {
+                  if(err) {
+                    //TODO: delete thread and message
+                    res.send(500);
+                  } else {
+                    res.send(200);
+                  }
+                });
+            } //if error insert pxm_message
+          }); //insert into pxm_message
+      } //if error inser pxm_thread
+    }); //insert into pxm_thread
+  };
+  
+  var error = function() {
+      res.send(401);
+  };
+  //TODO: post allowed?
+  //TODO: badwords
+  //TODO: html escaping
+  if(!req.session || !req.session.authenticated) {
+    if(req.body.username && req.body.password) {
+      checkLogin(req.body.user, req.body.password, post, error);
+    } else {
+      error();
+    }
+  } else {
+    post();
+  }
+  //messagecount fuer user erhoehen
+});
+
+/**
+ * Post an answer in a thread
+ **/
+pxm.post('/api/1/thread/:threadid/message', function(req, res, next) {
+  //if not logged in, use provided logindata
+  //post allowed?
+  //badwords
+  ///html escaping
+/*
+  INSERT INTO pxm_message (m_threadid,m_parentid,m_userid,m_usernickname,m_usermail,m_userhighlight,m_subject,m_body,m_tstmp,m_ip,m_notification)
+  UPDATE pxm_thread SET t_lastmsgtstmp=$this->m_iMessageTimestamp,t_lastmsgid=$this->m_iId,t_msgquantity=t_msgquantity+1 WHERE t_id=$this->m_iThreadId
+  UPDATE pxm_board SET b_lastmsgtstmp=$this->m_iMessageTimestamp WHERE b_id=$this->m_iBoardId
+
+  //Wenn autoclose und messagelimit erreicht
+  UPDATE pxm_thread SET t_active=0 WHERE t_id=$this->m_iThreadId AND t_msgquantity>=$iAutoClose
+*/
+  //messagecount fuer user erhoehen
+  //reply notification
 });
 
 pxm.listen(8080);
